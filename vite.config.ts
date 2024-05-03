c73d35c30ea1638d9d432961a13fc0fb985810b8
your_username <olumoyinjoshua19609@gmail.com>
2024-05-03 10:41:08 +0100

    commit all

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/budget',
   plugins: [react()],
   server:{
     port:3000,
